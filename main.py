1959b7fad34a2de1226977b84441471c56cc16ec
Diegome <diego02260@gmail.com>
2022-05-11 10:47:54 +0200

    Primero debemos quitar los corchetes despues de la variable coordenadas, ya que entonces pide un indice, y esta funcion solo admite valores "int"

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def punto_de_mira(lista_de_coordenadas):
     for coordenadas in lista_de_coordenadas:
-        plt.plot(coordenadas[0], coordenadas[1], 'bo')
+        plt.plot(coordenadas, coordenadas, 'bo')
 
     plt.plot(0, 0, 'k+')
     plt.ylabel('Y')
